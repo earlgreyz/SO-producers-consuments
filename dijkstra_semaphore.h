39b0877dd78e7fe49b16391c760ff323bef781e9
Mikołaj Walczak <mikiwalczak@gmail.com>
2017-03-14 23:06:54 +0100

    Files structure

---
+++ dijkstra_semaphore.h
@@ -0,0 +1,9 @@
+#ifndef DIJKSTRA_SEMAPHORE_H
+#define DIJKSTRA_SEMAPHORE_H
+
+#include <stdint.h>
+
+void proberen(int32_t *);
+void verhogen(int32_t *);
+
+#endif //DIJKSTRA_SEMAPHORE_H
---
+++ producer_consumer.h
@@ -0,0 +1,10 @@
+#ifndef PRODUCER_CONSUMER_H
+#define PRODUCER_CONSUMER_H
+
+#include <stddef.h>
+
+int init(size_t N);
+void producer(void);
+void consumer(void);
+
+#endif //PRODUCER_CONSUMER_H
