dce274ec1e0bdcde117e430891c95ab70b13db7e
Mikołaj Walczak <mikiwalczak@gmail.com>
2017-03-21 22:39:37 +0100

    Fix semaphore to avoid deadlocks and add comments

--- dijkstra_semaphore.asm
+++ dijkstra_semaphore.asm
@@ -4,14 +4,25 @@ global verhogen
 section .text
 
 spin:
+  ; Increase semaphore to compensate for the illegal decrease we made before
   lock inc dword [edi]
+
 proberen:
+  ; Wait until semaphore is greater than 0
+  cmp dword [edi], 0
+  jle proberen
+
+  ; Decrease semaphore by one
   mov esi, -1
   lock xadd [edi], esi
-  cmp esi, 1
-  jl spin
+
+  ; Spin if semaphore was not greater than 0
+  cmp esi, 0
+  jle spin
+
   ret
 
 verhogen:
+  ; Increase semaphore by one
   lock inc dword [edi]
   ret
